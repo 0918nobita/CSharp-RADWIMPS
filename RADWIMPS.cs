328f7cb38d06eebd6bc694b0732e12d7312598e6
0918nobita <nobita.0918@gmail.com>
2020-07-13 18:26:02 +0900

    :tada: Initial commit

---
+++ RADWIMPS.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+
+namespace RADWIMPS
+{
+    public class Program
+    {
+        private class RADWIMPS
+        {
+            private Task task = Task.CompletedTask;
+
+            public RADWIMPS then()
+            {
+                task = task.ContinueWith(_ => Console.Write("前"));
+                return this;
+            }
+
+            public Task 世()
+            {
+                task = task.ContinueWith(_ => Console.WriteLine("世"));
+                return task;
+            }
+        }
+
+        public static Task Main(string[] args)
+        {
+            return new RADWIMPS().then().then().then().世();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# C# で Then Then Then 世
+
+## 実行
+
+```bash
+dotnet run
+```
